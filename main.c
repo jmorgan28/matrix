befb5068b72ba9bdbd7957c45acb3865ebdcc87f
Jackson Morgan <jacksonmorgan@Jacksons-MacBook-Pro.local>
2017-02-27 11:47:28 -0500

    adding tests

--- main.c
+++ main.c
@@ -23,6 +23,16 @@ int main() {
 
   edges = new_matrix(4, 4);
   edges2 = new_matrix(4, 4);
+
+
+  printf("creating a matrix. Here is matrix one empty\n");
+
+  print_matrix(edges);
+
+ 
+  
+
+  
   //add_point(edges, 2, 4, 0);
   //add_point(edges, 99, 6, 0);
   // add_point(edges, 105, 507, 0);
@@ -39,6 +49,11 @@ int main() {
   add_edge(edges2,3,34,0,77,77,0);
   //draw_lines(edges, s,c);
 
+
+  printf("Here is matrix one filled\n");
+  print_matrix(edges);
+  printf("Here is matriz 2 filled\n");
+  print_matrix(edges2);
   //printf("check\n");
 
 
@@ -57,7 +72,17 @@ int main() {
 
   //grow_matrix(edges, 5);
 
+  printf("Matrix 2 after multiplication\n");
+  
   print_matrix(edges2);
+
+  printf("Matrix 2 scalar multiplied by 5\n");
+  scalar_mult(5, edges2);
+  print_matrix(edges2);
+
+  printf("Matrix 1 made into identity matrix\n");
+  ident(edges);
+  print_matrix(edges);
 
   
   //display(s);
